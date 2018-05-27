22693b958f1f16ef6a7e8a855a3a32864b78a5d8
Cry0lab <28391785+Cry0lab@users.noreply.github.com>
2018-05-27 16:34:49 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,41 +1,10 @@
 #!/bin/bash
-echo "Enter something: "
-read IP
-isAlive() #Takes an IP Address and sees if it is alive or not
-{
-A=$(ping -c 4 $1 | grep Unreach | wc -l )
-if [ "$A" != "0" ]; then
- echo "0" #ip is available
+#!/bin/bash
+
+ip="256.10.10.100"
+
+if [[ "$ip" =~ (([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5]))$ ]]; then
+  echo "success"
 else
- echo "1" #Ip is alive
+  echo "fail"
 fi
-}
-
-{
-I=$IP
-
-#if [ "$(ipcalc -cs $I && echo 1 || echo 0)" == 0 ]; then
-#echo "That is not a valid IP Address. Please enter a valid IP Address:"
-#read IP
-#validIP
-#return 0
-#fi
-
-if [[ "$ip" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
-  echo "failure"
-  read IP
-  validIP
-  return 0
-fi
-
-if [ "$(isAlive $I)" == "1" ] && [ "$I" != "$DHCP_IP" ]; then
- echo "That IP ADDRESS is already in use. Please enter a different one:"
- read IP
- validIP
- return 0
-fi
-
-
-}
-
-validIP
